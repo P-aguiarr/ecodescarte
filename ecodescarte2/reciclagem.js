633c44dedc0dabdfef1c4bcae6f495ea92adfedc
Paula Aguiar <82782637+P-aguiarr@users.noreply.github.com>
2024-10-27 20:48:16 -0300

    Delete ecodescarte2 directory

--- ecodescarte2/reciclagem.js
+++
@@ -1,8 +0,0 @@
-function showDetails(centerId) {
-    const details = document.getElementById(centerId);
-    if (details.style.display === "none") {
-        details.style.display = "block";
-    } else {
-        details.style.display = "none";
-    }
-}
